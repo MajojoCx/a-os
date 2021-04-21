8a468492cd9f9834290f3458b8fc44e6b152dc3d
MR GB <MR GB@LAPTOP-QJF7050U>
2021-04-20 20:57:39 -0500

    :DDDDDDd

---
+++ src/actividad/pkg1/pkgif/pkg60008/Actividad1If60008.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad.pkg1.pkgif.pkg60008;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 60008
+ */
+public class Actividad1If60008 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        Scanner entrada = new Scanner(System.in);
+       
+       System.out.println("Introduce un año");
+       int año = entrada.nextInt();
+       int añoD = 0;
+       
+       if(año < 2021){
+           añoD = 2021 - año;
+           System.out.println("Han pasado " + añoD + " años");
+       } else if(año > 2021){
+           añoD = año - 2021;
+           System.out.println("Faltan " + añoD + " años");
+       } else{
+           System.out.println("Estamos en el mismo año");
+    }   
+}
+}
